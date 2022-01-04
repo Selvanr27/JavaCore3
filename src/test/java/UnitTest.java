28637d2a8a8f2b81316b2d90c220698339aceb3a
selvan.r <selvan.r@collabera.com>
2022-01-04 15:47:22 +0530

    4th

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class UnitTest {
+    @DisplayName("Check variable values are correct")
+    @Test
+    void learnTheVariablesValidCase() {
+        int osNum = 12;
+        int expectedValue = 12;
+        Assertions.assertEquals(expectedValue, osNum);
+    }
+
+    @DisplayName("Check variable values are incorrect")
+    @Test
+    void learnTheVariablesInValidCase() {
+        int osNum = 12;
+        int expectedValue = 13;
+        Assertions.assertNotEquals(expectedValue, osNum);
+    }
+}
