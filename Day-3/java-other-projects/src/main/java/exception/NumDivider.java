8f0e4ab2307efc1d0708fa418c47b79213ed2a69
selvan.r <selvan.r@collabera.com>
2022-01-05 13:51:28 +0530

    java oop:Generic,exception

---
+++ Day-3/java-other-projects/src/main/java/exception/NumDivider.java
@@ -0,0 +1,31 @@
+package exception;
+
+public class NumDivider {
+
+    public double divideNumbers(double num1, double num2) {
+        if(num2 == 0)
+            return 0;
+        return num1/num2;
+    }
+
+    public double divideNumbersException(double num1, double num2) {
+        if(num2 == 0) throw new BadNumberException("num2 is 0");
+        return num1/num2; // throw new ArithmeticException()
+    }
+
+    public void divideNumberHandle(double num1, double num2) {
+
+        try {
+            double res = num1/num2;
+        }catch (ArithmeticException e) {
+            double res = 0;
+        }catch (RuntimeException e) {
+
+        }
+        catch (Exception e) {
+
+        }
+        finally {
+        }
+    }
+}
