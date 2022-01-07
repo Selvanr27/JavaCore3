8d10c76d1a0ccb69ed4c441608309e52e79061f1
selvan.r <selvan.r@collabera.com>
2022-01-07 15:11:46 +0530

    Factory

---
+++ Day-5/src/main/java/factory/CoinFactory.java
@@ -0,0 +1,17 @@
+package factory;
+package factory;
+
+public class CoinFactory extends  Factory{
+
+    private CoinFactory() {
+    }
+
+    public static CoinFactory newInstance() {
+        return new CoinFactory();
+    }
+
+    public static CoinProvider getCoin(String bank) {
+        if(bank.equals("abc")) return new AbcCoin();
+        throw new InvalidBankException("Invalid Bank");
+    }
+}
---
+++ Day-5/src/test/java/CarProviderTests.java
@@ -0,0 +1,2 @@
+public class CarProviderTests {
+}
