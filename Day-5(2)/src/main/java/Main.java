fa13cabe5438977776e9f88070270087d39d0a37
selvan.r <selvan.r@collabera.com>
2022-01-07 17:50:26 +0530

    Spring : Car,tempo,driver

--- Day-5(2)/src/main/java/Main.java
+++ Day-5(2)/src/main/java/Main.java
@@ -1,29 +1,8 @@
-//import car.Car;
-
-//public class Main {
-
- //   public static void main(String[] args) {
-
-   //     Car car = new Car();
-
-        //if you need want to create object of driver
-        // you need to have object car
-        // which also means
-        // driver object is dependent on car object
-
-        // dependency can be satisfied in 2 ways
-        // first supplying car in a constructor
-        // supplying car in a setter method
-
-        // Driver driver = new Driver(car);
-
- //       Driver driver = new Driver();
- //       driver.setCar(car);
-//    }
-//}
 import car.Car;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 public class Main {
 
@@ -31,12 +10,13 @@ public class Main {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        Car car1 = (Car) context.getBean("carakfjhlskfhslhlshg");
+        Car car1 = (Car) context.getBean("car");
         car1.setSpeed(100);
 
         Class<Car> cls = Car.class; // this is not the bytecode class file
         Car car2 = context.getBean(cls);
         System.out.println("Car Speed is "+ car2.getSpeed());
 
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
