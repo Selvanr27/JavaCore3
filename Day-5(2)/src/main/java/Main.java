7694b346539b6099761829b00157477c7d46283e
selvan.r <selvan.r@collabera.com>
2022-01-07 17:22:11 +0530

    Spring : Car speed

--- Day-5(2)/src/main/java/Main.java
+++ Day-5(2)/src/main/java/Main.java
@@ -1,11 +1,10 @@
-import car.Car;
-import car.Driver;
+//import car.Car;
 
-public class Main {
+//public class Main {
 
-    public static void main(String[] args) {
+ //   public static void main(String[] args) {
 
-        Car car = new Car();
+   //     Car car = new Car();
 
         //if you need want to create object of driver
         // you need to have object car
@@ -18,7 +17,26 @@ public class Main {
 
         // Driver driver = new Driver(car);
 
-        Driver driver = new Driver();
-        driver.setCar(car);
+ //       Driver driver = new Driver();
+ //       driver.setCar(car);
+//    }
+//}
+import car.Car;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Car car1 = (Car) context.getBean("carakfjhlskfhslhlshg");
+        car1.setSpeed(100);
+
+        Class<Car> cls = Car.class; // this is not the bytecode class file
+        Car car2 = context.getBean(cls);
+        System.out.println("Car Speed is "+ car2.getSpeed());
+
     }
 }
