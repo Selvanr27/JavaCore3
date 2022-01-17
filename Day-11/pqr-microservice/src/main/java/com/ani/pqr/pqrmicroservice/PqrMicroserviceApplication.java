d82a124e595c2925d81c34e60e8011b4fddeccd9
selvan.r <selvan.r@collabera.com>
2022-01-17 14:24:04 +0530

    Second

---
+++ Day-11/pqr-microservice/src/main/java/com/ani/pqr/pqrmicroservice/PqrMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ani.pqr.pqrmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Day-11/pqr-microservice/src/test/java/com/ani/pqr/pqrmicroservice/PqrMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.pqr.pqrmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
