792b3b0d845c593017fd2c06e7c1da0da17561f1
selvan.r <selvan.r@collabera.com>
2022-01-23 12:50:16 +0530

    Final

--- Billing-module/src/test/java/com/tamil/billing/repository/BillRepositoryTests.java
+++ Billing-module/src/test/java/com/tamil/billing/repository/BillRepositoryTests.java
@@ -2,6 +2,7 @@ package com.tamil.billing.repository;
 
 
 import com.tamil.billing.domain.Bill;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -18,11 +20,7 @@ import java.time.LocalDate;
 
 public class BillRepositoryTests {
 
-
-
-
-
-        @Autowired
+    @Autowired
         private BillRepository repository;
 
         @DisplayName("Bill Repository--> Creating Bill")
@@ -37,5 +35,27 @@ public class BillRepositoryTests {
 
             repository.save(bill);
         }
+
+    @DisplayName("Bill Repository--> get Bill")
+    @Test
+    public void getBillTest() {
+
+            List<Bill> bill= repository.findAll();
+        Assertions.assertEquals(bill.size(),17);
+
+    }
+
+    @DisplayName("Bill Repository--> Update Bill")
+    @Test
+    public void updateBillTest() {
+            Bill bill=repository.findById(13l).get();
+            bill.setPatientName("selvan");
+            Bill billUpdated=repository.save(bill);
+            Assertions.assertNotEquals(billUpdated.getPatientName(),bill);
+
+
+    }
+
+
     }
 
